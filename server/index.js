960218275eef74071a6564d613c0a574f157ffc7
Derrick Morton <creationator@users.noreply.github.com>
2019-07-10 12:55:09 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use('/:id', express.static(__dirname + '/public'));
+// Review this line below to make sure I know what it is doing.
 app.use('/', proxy({ target: 'http://localhost:3002', changeOrigin: true }))
 
 app.listen(3202, () => {
